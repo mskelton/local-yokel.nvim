a84c1e9e8648dfae0a08c462c49f03b8ab4a28ee
Mark Skelton <mdskelton99@gmail.com>
2023-04-12 06:41:38 -0500

    Don't go to unrelated files

--- lua/local-yokel/relatives.lua
+++ lua/local-yokel/relatives.lua
@@ -2,13 +2,8 @@ local config = require("local-yokel.config")
 
 local M = {}
 
---- @param glob string
---- @return table
-local function read_dir(glob)
-	local dir = vim.fn.expand("%:p:h")
-	return vim.fn.globpath(dir, glob, true, true)
-end
-
+--- @param tbl table
+--- @param val any
 local function find_index(tbl, val)
 	for i, v in ipairs(tbl) do
 		if v == val then
@@ -17,12 +12,13 @@ local function find_index(tbl, val)
 	end
 end
 
-local function get_file()
+local function get_current_file()
 	return vim.fn.expand("%:p")
 end
 
-local function get_file_prefix()
-	local prefix = string.gsub(vim.fn.expand("%:t"), "%..*$", "")
+--- @param file string
+local function get_file_prefix(file)
+	local prefix = string.gsub(file, "%..*$", "")
 	local postfixes = config.get("relatives.postfixes")
 
 	for _, postfix in pairs(postfixes or {}) do
@@ -32,6 +28,23 @@ local function get_file_prefix()
 	return prefix
 end
 
+--- @param prefix string?
+local function read_dir(prefix)
+	local dir = vim.fn.expand("%:p:h")
+	local glob = (prefix or "") .. "*"
+	local res = vim.fn.globpath(dir, glob, true, true)
+
+	if prefix == nil then
+		return res
+	end
+
+	return vim.tbl_filter(function(file)
+		return prefix == get_file_prefix(vim.fn.fnamemodify(file, ":t"))
+	end, res)
+end
+
+--- @param dir table
+--- @param index number
 local function navigate(dir, index)
 	-- No need to navigate when this is the only file
 	if #dir < 2 then
@@ -48,26 +61,28 @@ local function navigate(dir, index)
 end
 
 M.next = function()
-	local dir = read_dir("*")
-	local index = find_index(dir, get_file())
-	navigate(index + 1)
+	local dir = read_dir()
+	local index = find_index(dir, get_current_file())
+	navigate(dir, index + 1)
 end
 
 M.prev = function()
-	local dir = read_dir("*")
-	local index = find_index(dir, get_file())
-	navigate(index - 1)
+	local dir = read_dir()
+	local index = find_index(dir, get_current_file())
+	navigate(dir, index - 1)
 end
 
 M.next_sibling = function()
-	local dir = read_dir(get_file_prefix() .. "*")
-	local index = find_index(dir, get_file())
+	local current_file = vim.fn.expand("%:t")
+	local dir = read_dir(get_file_prefix(current_file))
+	local index = find_index(dir, get_current_file())
 	navigate(dir, index + 1)
 end
 
 M.prev_sibling = function()
-	local dir = read_dir(get_file_prefix() .. "*")
-	local index = find_index(dir, get_file())
+	local current_file = vim.fn.expand("%:t")
+	local dir = read_dir(get_file_prefix(current_file))
+	local index = find_index(dir, get_current_file())
 	navigate(dir, index - 1)
 end
 
